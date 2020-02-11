275a167440183733d509fbf8558e20e25423b93e
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-11 16:30:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  new_array = []
+  name_array.each do |name|
+    new_array.push(badge_maker(name))
+  end
+  new_array
+end
+

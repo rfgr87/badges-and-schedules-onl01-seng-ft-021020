b5a4542376ca4384b4dc7279111dc0d8a7bc36ab
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-11 16:40:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,3 +12,27 @@ def batch_badge_creator(name_array)
   new_array
 end
 
+def assign_rooms(name_array)
+  room = 1 
+  list = []
+  name_array.each do |name|
+    list.push("Hello, #{name}! You'll be assigned to room #{room}!")
+    room += 1 
+  end
+  list
+end
+
+def printer(name_array)
+  array1 = []
+  array2 = []
+  array3 = []
+  array1 = batch_badge_creator(name_array)
+  array2 = assign_rooms(name_array)
+  array1.each do |message|
+    array3.push(message)
+  end
+  array2.each do |message|
+    array3.push(message)
+  end
+  array3
+end
